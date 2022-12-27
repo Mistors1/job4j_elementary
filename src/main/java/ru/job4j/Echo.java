983679fe0118087e1d0278ce34a312aa5b700e86
Mistors1 <Kolov-dm@bk.ru>
2022-12-27 19:29:25 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,11 @@
 package ru.job4j;
 
 public class Echo {
+    public static void main(String[] arg) {
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+
+    }
 }
