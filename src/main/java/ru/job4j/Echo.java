3d28ec239f1602908acf2c442d7912345200a1ab
Mistors1 <Kolov-dm@bk.ru>
2022-12-29 17:44:00 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,11 +1,15 @@
 package ru.job4j;
 
 public class Echo {
-    public static void main(String[] arg) {
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        System.out.println(size);
-
+    public static void plus(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
     }
+
+    public static void main(String[] args) {
+        Echo.plus(100, 500);
+        Echo.plus(4, 2);
+        Echo.plus(3, 5);
+    }
+
 }
