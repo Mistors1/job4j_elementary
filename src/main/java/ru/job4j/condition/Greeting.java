b6ee7febc7cd5314089c81fcf42b370338716204
Mistors1 <Kolov-dm@bk.ru>
2023-01-02 16:24:09 +0300

    Greeting

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = " I like jawa";
+        idea = idea + " But i am newbie";
+        int year = 2023;
+        idea += year;
+        System.out.println(idea);
+    }
+}
