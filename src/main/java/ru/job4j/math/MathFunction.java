e5c46f9196891862f08dcba94283227a21bcbe54
Mistors1 <Kolov-dm@bk.ru>
2023-01-03 18:15:47 +0300

    MathFunction

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,23 @@
+package ru.job4j.math;
+
+public class MathFunction {
+    public static double sum(double a, double b) {
+        double sum = a + b;
+        return sum;
+    }
+
+    public static double multiply(double a, double b) {
+        double multiply = a * b;
+        return multiply;
+    }
+
+    public static double minus(double a, double b) {
+        double minus = a - b;
+        return minus;
+    }
+
+    public static double divide(double a, double b) {
+        double divide = a / b;
+        return divide;
+    }
+}
