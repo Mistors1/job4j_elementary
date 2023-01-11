4e890dd65604f763aa360d104f38c99ba56e2ce2
Mistors1 <Kolov-dm@bk.ru>
2023-01-11 20:14:30 +0300

    Loop

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,11 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) {
+
+    }
+
+    short[] ages = new short[10];
+    String[] surnames = new String[100500];
+    float[] prices = new float[40];
+}
