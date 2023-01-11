5f4f5d3cfee43d76417dd78ba21ae0049a1894cf
Mistors1 <Kolov-dm@bk.ru>
2023-01-11 19:05:39 +0300

    AlgoArray

--- src/test/java/ru/job4j/array/AlgoArray.java
+++ src/test/java/ru/job4j/array/AlgoArray.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
+        int[] array = new int[]{5, 3, 2, 1, 4};
         int temp = array[0];
         array[0] = array[3];
         array[3] = array[4];
@@ -11,8 +11,8 @@ public class AlgoArray {
         array[1] = array[4];
         array[4] = temp;
 
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+        for (int index : array) {
+            System.out.println(index);
         }
     }
 }
